61287f761fa8fd779c35300f9321debc21ddc90e
Jacob <104500838+JustExisting1@users.noreply.github.com>
2024-06-27 19:15:36 +0100

    Test new experimental build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: "/tw-portfolio/",
+    base: "/tw-portfolio/",
     build: { outDir: "dist" },
-    publicDir: "public"
+    publicDir: "public",
+    experimental: {
+        renderBuiltUrl: function (filename, _a) {
+            var hostType = _a.hostType;
+            if (hostType === "js") {
+                return {
+                    runtime: "window.__toCdnUrl(".concat(JSON.stringify(filename), ")"),
+                };
+            }
+            else {
+                return { relative: true };
+            }
+        },
+    },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // base: "/tw-portfolio/",
-  build:{outDir:"dist"},
-  publicDir:"public"
-})
+    plugins: [react()],
+    base: "/tw-portfolio/",
+    build: { outDir: "dist" },
+    publicDir: "public",
+    experimental: {
+        renderBuiltUrl(filename, { hostType }) {
+            if (hostType === "js") {
+                return {
+                    runtime: `window.__toCdnUrl(${JSON.stringify(filename)})`,
+                };
+            } else {
+                return { relative: true };
+            }
+        },
+    },
+});
