c0fd71e87684737697fe9287b78f123bc4a4849a
Jacob <104500838+JustExisting1@users.noreply.github.com>
2024-06-27 19:01:15 +0100

    Rebuild

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    // base: "/tw-portfolio/",
-    build: { outDir: "dist" },
-    publicDir: "public"
-});
