4704f83d0eba0105f72ebb5896863c7def3ef562
Jacob <104500838+JustExisting1@users.noreply.github.com>
2024-06-27 19:02:38 +0100

    Without Base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tw-portfolio/",
+  // base: "/tw-portfolio/",
   build:{outDir:"dist"},
   publicDir:"public"
 })
